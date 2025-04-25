48c955575a81b36f65408b9b84af66d3cc8ca43e
Gal Ben David <gal.benda3@gmail.com>
2025-04-25 20:13:34 +0300

    Add Activities component

--- cmps/about/About.tsx
+++ cmps/about/About.tsx
@@ -1,15 +1,25 @@
+'use client'
 import './About.css'
 import { SocialLinks } from '../social-links/SocialLinks'
+import { useEffect, useState } from 'react'
 
 export function About() {
+
+    const [opacity, setOpacity] = useState(false)
+
+    useEffect(() => {
+        setOpacity(true)
+    }, [])
+
+
     return (
         <section className="about">
-            <div className="profile-img">
+            <div className={`profile-img`} style={{ opacity: opacity ? '1' : '0', top: opacity ? '-9%' : '-25%' }}>
                 <img src="https://res.cloudinary.com/dvykycdey/image/upload/v1745423327/vered-img_genget.jpg" />
             </div>
 
             <div className="content about-content">
-                <div>
+                <div style={{ opacity: opacity ? '1' : '0', transition: '2s ease-in-out' }}>
                     <h1 className="rtl">ורד בן דוד</h1>
                     <h2 className="rtl">נומרולוגיה, ייעוץ והכוונה</h2>
                     <h2 className="rtl">פגישות אישיות ・ סדנאות קבוצתיות</h2>
--- cmps/activities/Activities.tsx
+++ cmps/activities/Activities.tsx
@@ -2,24 +2,67 @@ import './Activities.css'
 
 import Image from "next/image";
 
-// const WhatsappIcon = () => (
-//     <Image
-//         src="https://res.cloudinary.com/dvykycdey/image/upload/v1745515614/vered-workshop_gx6vsk.jpg"
-//         alt="workshop"
-//         height={100}
-//     />
-// )
+const workshopList = [
+    {
+        title: 'בעקבות המספרים',
+        img: <Image
+            src="https://res.cloudinary.com/dvykycdey/image/upload/v1745598978/numbers_1_zvy7ye.jpg"
+            alt="workshop"
+            height={260}
+            width={300}
+        />
+    },
+    {
+        title: 'חכמת נשים',
+        img: <Image
+            src="https://res.cloudinary.com/dvykycdey/image/upload/v1745598978/women-bible_qxhlkx.jpg"
+            alt="workshop"
+            height={260}
+            width={300}
+        />
+    },
+    {
+        title: 'בניית חזון אישי',
+        img: <Image
+            src="https://res.cloudinary.com/dvykycdey/image/upload/v1745598978/vision_1_tct4wp.jpg"
+            alt="workshop"
+            height={260}
+            width={300}
+        />
+    },
+    {
+        title: 'הקול בראש',
+        img: <Image
+            src="https://res.cloudinary.com/dvykycdey/image/upload/v1745598978/inner_voice_1_dgk3fq.jpg"
+            alt="workshop"
+            height={260}
+            width={300}
+        />
+    },
+
+]
 
 export function Activities() {
     return (
         <section className="activities full main-layout">
-            <p className="rtl">נומרולוגית מובילה בארץ,</p>
-            <p className="rtl"> מקדמת אנשים להתפתחות והעצמה אישית ✨</p>
-            <ul>
-                <li>
-                    <img src="https://res.cloudinary.com/dvykycdey/image/upload/v1745515614/vered-workshop_gx6vsk.jpg" />
-                </li>
-            </ul>
+            <div>
+                <p className="rtl">נומרולוגית מובילה בארץ,</p>
+                <p className="rtl"> מקדמת אנשים להתפתחות והעצמה אישית ✨</p>
+            </div>
+
+            <div>
+                <h2>רשימת סדנאות</h2>
+                <ul className="activities-list">
+                    {
+                        workshopList.map((item, i) =>
+                            <li key={i}>
+                                <h2>{item.title}</h2>
+                                {item.img}
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
 
         </section>
     )
