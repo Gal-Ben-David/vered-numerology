362948491459a5d2b15861081b180c43162dd9fb
Gal Ben David <gal.benda3@gmail.com>
2025-04-27 12:33:14 +0300

    Update styling

--- cmps/call-to-action/CallToAction.tsx
+++ cmps/call-to-action/CallToAction.tsx
@@ -3,7 +3,7 @@ import './CallToAction.css'
 export function CallToAction() {
 
     return (
-        <section className="call-to-action">
+        <section className="call-to-action full">
             <p className="rtl large">סקרנים לדעת מה המספרים אומרים עליכם?</p>
             <p className="rtl large">בואו נדבר 👇</p>
         </section>
