ce10f3f54e005d2edcdccd36f29a35ae08427620
Gal Ben David <gal.benda3@gmail.com>
2025-04-29 17:50:19 +0300

    Add form in Calculator component

--- cmps/calculator/Calculator.tsx
+++ cmps/calculator/Calculator.tsx
@@ -1,7 +1,10 @@
+'use client'
 import { DateIcon } from '@/data/icons'
 import './Calculator.css'
+import { useState } from 'react'
 
 export function Calculator() {
+    const [date, setDate] = useState<number | null>(0)
     return (
         <section className="calculator full main-layout rtl">
             <h2>מחשבון שביל גורל</h2>
@@ -16,6 +19,24 @@ export function Calculator() {
                 <p>הכניסו את תאריך הלידה וגלו מהו מספר הגורל שמלווה אתכם</p>
             </div>
 
+            <form className="date-calculator-form">
+                <div className="date-inputs">
+                    <label className="date-input">
+                        <span>יום</span>
+                        <input className="day-input" type="number" inputMode="numeric" pattern="[0-9]*" placeholder="DD" required />
+                    </label>
+                    <label className="date-input">
+                        <span>חודש</span>
+                        <input className="month-input" type="number" inputMode="numeric" pattern="[0-9]*" placeholder="MM" required />
+                    </label>
+                    <label className="date-input">
+                        <span>שנה</span>
+                        <input className="year-input" type="number" inputMode="numeric" pattern="[0-9]*" placeholder="YYYY" required />
+                    </label>
+                </div>
+
+                <button className="btn bold btn-calculator">חשב</button>
+            </form>
             <hr />
         </section>
     )
